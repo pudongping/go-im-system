8efc1b4f66f617bf3104b6f6117b86e910486101
pudongping <276558492@qq.com>
2021-12-12 00:55:02 +0800

    菜单选择

--- client.go
+++ client.go
@@ -11,6 +11,7 @@ type Client struct {
 	ServerPort int    // 链接端口
 	Name       string
 	conn       net.Conn // 链接句柄
+	flag       int      // 当前 client 的模式
 }
 
 func NewClient(serverIp string, serverPort int) *Client {
@@ -18,6 +19,7 @@ func NewClient(serverIp string, serverPort int) *Client {
 	client := &Client{
 		ServerIp:   serverIp,
 		ServerPort: serverPort,
+		flag:       999,
 	}
 
 	// 链接 server
@@ -32,6 +34,51 @@ func NewClient(serverIp string, serverPort int) *Client {
 
 	// 返回对象
 	return client
+}
+
+func (c *Client) menu() bool {
+	var flag int
+
+	fmt.Println("1.公聊模式")
+	fmt.Println("2.私聊模式")
+	fmt.Println("3.更新用户名")
+	fmt.Println("0.退出")
+
+	// 等待用户键盘输入
+	fmt.Scanln(&flag)
+
+	if flag >= 0 && flag <= 3 {
+		c.flag = flag
+		return true
+	} else {
+		fmt.Println(">>>>请输入合法范围内的数字<<<<")
+		return false
+	}
+
+}
+
+func (c *Client) Run() {
+	if c.flag != 0 {
+		for c.menu() != true {
+
+		}
+
+		// 根据 flag 不同的模式处理不同的业务
+		switch c.flag {
+		case 1:
+			// 公聊模式
+			fmt.Println("公聊模式选择")
+			break
+		case 2:
+			// 私聊模式
+			fmt.Println("私聊模式选择")
+			break
+		case 3:
+			// 更新用户名
+			fmt.Println("更新用户名选择")
+			break
+		}
+	}
 }
 
 var serverIp string
@@ -59,5 +106,5 @@ func main() {
 	fmt.Println(">>>>>>>> 链接服务器成功……")
 
 	// 启动客户端的业务
-	select {}
+	client.Run()
 }
